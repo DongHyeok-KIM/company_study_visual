90a6d9f18b91508f12921b613e95c3f2fa29ff4d
DongHyeok-KIM <donut_nv@naver.com>
2022-02-06 15:12:59 +0900

    시각화화면 웹페이지화(대시보드)

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route('/')
+def root():
+    return 'welcome to flask'
+
+@app.route('/sub')
+def sub():
+    return 'sub page'
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
