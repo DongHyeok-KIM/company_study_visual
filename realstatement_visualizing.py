bb361ec7bbc09114380ffea22b85efb156de7fca
DongHyeok-KIM <donut_nv@naver.com>
2022-02-08 01:32:10 +0900

    #소스코드 업로드

--- realstatement_visualizing.py
+++ realstatement_visualizing.py
@@ -15,6 +15,9 @@ from flask_restful import reqparse
 class Realstatement_visualizing:
     def __init__(self):
         self.president_data = pd.DataFrame()
+        self.main : str
+        self.sub : list
+        self.ytic_name : str
         self.file_name : str
         plt.rcParams['axes.unicode_minus']= False
         if platform.system() == 'Darwin': #맥os 사용자의 경우
@@ -22,16 +25,10 @@ class Realstatement_visualizing:
             rc('font', family = 'AppleGothic')
 
     def file_name_parser(self):
-        print("1")
         parser = reqparse.RequestParser()
         parser.add_argument('file_name', required=True, type=str)
-        print(2, parser)
         args = parser.parse_args()
-        print(3, args)
         self.file_name = str(args['file_name'])
-        print(self.file_name)
-        print("4")
-
         return self.file_name
 
     def load_csv_file(self, file_name):
@@ -57,6 +54,10 @@ class Realstatement_visualizing:
         df['구분'] = df['구분'].apply(lambda x: x.replace("[%]", ""))
         df['구분'] = df['구분'].apply(lambda x: x.replace("[백만원]", ""))
         df['구분'] = df['구분'].apply(lambda x: x.replace("[2019.01 = 100]", ""))
+        if df['값'].max()>1000000:
+            df['값'] = df['값'].apply(lambda x:x/1000000)
+        df.rename(columns={'값':f'{self.ytic_name}'},inplace=True)
+
 
         return df
 
@@ -73,21 +74,16 @@ class Realstatement_visualizing:
             plt.text(inaug_date + relativedelta(years=y, month=m), y=max_value, s=president, fontsize=11)
 
     def lineplot_presidentline(self, df, name):
-        print(df.head())
         temp_df = df.query('구분 == @name')
-        print("lineplot_presidentline")
-        print(name)
-        print(temp_df)
-        max_value = temp_df['값'].max() * 0.95
-        sns.lineplot(data=temp_df, x='년월', y=f'값', hue='구분')
-        plt.title(f'총계', fontsize=20, fontweight='bold')
+        max_value = temp_df[f'{self.ytic_name}'].max() * 0.95
+        sns.lineplot(data=temp_df, x='년월', y=f'{self.ytic_name}', hue='구분')
+        plt.title(f'{self.file_name}({self.main})_시각화 그래프', fontsize=20, fontweight='bold')
         plt.legend(loc='upper left', bbox_to_anchor=(0.05, 0.9))
         self.presidentline(presidents_inaug_dates, max_value)
 
     def lineplot(self, df, 리스트, title):
         temp_df = df.query('구분 in @ 리스트')
-        max_value = temp_df['값'].max() * 0.95
-        sns.lineplot(data=temp_df, x='년월', y=f'값', hue='구분')
+        sns.lineplot(data=temp_df, x='년월', y=f'{self.ytic_name}', hue='구분')
         plt.title(f'{title} 상세', fontsize=15, fontweight='bold')
         plt.xticks(rotation=45)
 
@@ -107,62 +103,51 @@ class Realstatement_visualizing:
         p.set_clip_on(False)
         return c1, c2, bbox_patch1, bbox_patch2, p
 
-    def zoom_effect02(self, ax1, ax2, **kwargs):
+    def zoom_effect(self, ax1, ax2, **kwargs):
         tt = ax1.transScale + (ax1.transLimits + ax2.transAxes)
         trans = blended_transform_factory(ax2.transData, tt)
-
         mybbox1 = ax1.bbox
         mybbox2 = TransformedBbox(ax1.viewLim, trans)
-
         prop_patches = kwargs.copy()
         prop_patches["ec"] = "none"
         prop_patches["alpha"] = 0.2
-
         c1, c2, bbox_patch1, bbox_patch2, p = self.connect_bbox(mybbox1, mybbox2, loc1a=2, loc2a=3, loc1b=1, loc2b=4, prop_lines=kwargs, prop_patches=prop_patches)
-
         ax1.add_patch(bbox_patch1)
         ax2.add_patch(bbox_patch2)
         ax2.add_patch(c1)
         ax2.add_patch(c2)
         ax2.add_patch(p)
-
         return c1, c2, bbox_patch1, bbox_patch2, p
 
-
-
-    def main_sub_choice(self, file_name):
+    def ytic_main_sub_choice(self, file_name):
         if file_name == '국내건설수주액':
-            main = '총수주액'
-            sub = ['공공부문', '민간부문']
-        elif file_name =='주택매매지수':
-            main = '총지수'
-            sub = ['단독주택','연립주택','아파트']
-        else:
-            main = '전국'
-            sub = ['서울', '경기', '제주']
-        return main, sub
+            self.main = '총수주액'
+            self.sub = ['공공부문', '민간부문']
+            self.ytic_name = '수주액(조)'
+        elif file_name =='주택매매가격지수(KB)':
+            self.main = '총지수'
+            self.sub = ['단독주택','연립주택','아파트']
+            self.ytic_name = '매매지수(2019.01=100)'
+        elif file_name == '지가변동률':
+            self.main = '전국'
+            self.sub = ['서울', '경기', '제주']
+            self.ytic_name = '변동률(%)'
+        #return file_name, ytic_name, main, sub
 
     def triple_grap(self):
-        print("triple_grap", self.file_name)
         df = self.load_csv_file(self.file_name)
-        print(self.file_name)
-        print(df.head())
+        self.ytic_main_sub_choice(self.file_name)
         df = self.data_preprocessing(df)
-        print("전처리")
-        print(df.head())
-
-        main, sub = self.main_sub_choice(self.file_name)
-        print(main, sub)
+        #main, sub = self.main_sub_choice(self.file_name)
         plt.figure(figsize=(15, 10))
         ax1 = plt.subplot(2, 1, 1)
-        self.lineplot_presidentline(df, main)
+        self.lineplot_presidentline(df, self.main)
         ax2 = plt.subplot(2, 2, 3)
-        self.lineplot(df.query(f'"2008-03-01" <= 년월 < "2013-03-01"'), sub, '이명박임기')
-        self.zoom_effect02(ax2, ax1)
+        self.lineplot(df.query(f'"2008-03-01" <= 년월 < "2013-03-01"'), self.sub, '이명박임기')
+        self.zoom_effect(ax2, ax1)
         ax3 = plt.subplot(2, 2, 4)
-        self.lineplot(df.query(f'"2017-05-01" <= 년월'), sub, '문재인임기')
-        self.zoom_effect02(ax3, ax1)
+        self.lineplot(df.query(f'"2017-05-01" <= 년월'), self.sub, '문재인임기')
+        self.zoom_effect(ax3, ax1)
         return plt
-        # plt.show()
 
 
